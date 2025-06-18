0d21c144050acf55ceabd3dccebe5eaf59b0f786
Ryan Eno <ryan.eno@elastic.co>
2025-06-17 19:42:32 -0700

    Add parsing for bulk task descriptions

--- src/processor/elasticsearch/tasks.rs
+++ src/processor/elasticsearch/tasks.rs
@@ -20,21 +20,19 @@ impl DataProcessor<Lookups, ElasticsearchMetadata> for Tasks {
         let lookup_node = &lookups.node;
         let task_metadata = metadata.for_data_stream(&data_stream).as_meta_doc();
 
-        let nodes: Vec<(String, NodeTasks)> = self.nodes.into_par_iter().collect();
+        let mut nodes: Vec<(String, NodeTasks)> = self.nodes.into_par_iter().collect();
 
         let tasks: Vec<Value> = nodes
-            .into_par_iter()
-            .flat_map(|(node_id, node)| {
+            .par_drain(..)
+            .flat_map(|(node_id, mut node)| {
                 node.tasks
-                    .iter()
-                    .collect::<Vec<_>>()
-                    .into_par_iter()
+                    .par_drain()
                     .map(|(_, task)| {
                         let node = lookup_node
                             .by_id(node_id.as_str())
                             .cloned()
                             .expect("Node not found for task");
-                        serde_json::to_value(TaskDoc::new(task, task_metadata.clone(), node))
+                        serde_json::to_value(EnrichedTask::new(task, task_metadata.clone(), node))
                             .unwrap_or_default()
                     })
                     .collect::<Vec<Value>>()
@@ -47,28 +45,57 @@ impl DataProcessor<Lookups, ElasticsearchMetadata> for Tasks {
 }
 
 #[derive(Clone, Serialize)]
-pub struct TaskDoc {
+pub struct EnrichedTask {
     #[serde(flatten)]
     metadata: Value,
     node: NodeDocument,
     task: TaskWithParent,
+    #[serde(flatten)]
+    data: Option<TaskData>,
 }
 
-impl TaskDoc {
-    pub fn new(task: &Task, metadata: Value, node: NodeDocument) -> Self {
+impl EnrichedTask {
+    pub fn new(task: Task, metadata: Value, node: NodeDocument) -> Self {
         let parent = task
             .parent_task_id
             .as_ref()
             .map(|id| ParentTask::from(id.clone()));
-        TaskDoc {
+        EnrichedTask {
             metadata: metadata.clone(),
             node,
-            task: TaskWithParent {
-                task: task.clone(),
-                parent,
-            },
+            data: TaskData::new((task.action.clone(), task.description.clone())),
+            task: TaskWithParent { task, parent },
         }
     }
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Serialize)]
+pub struct TaskData {
+    shard: Option<TaskShard>,
+    index: Option<TaskIndex>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Serialize)]
+pub struct TaskShard {
+    number: Option<u32>,
+    primary: Option<bool>,
+    bulk: Option<BulkDocs>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Serialize)]
+pub struct TaskIndex {
+    count: Option<u32>,
+    name: Option<Vec<String>>,
+    bulk: Option<BulkDocs>,
+}
+
+#[skip_serializing_none]
+#[derive(Clone, Serialize)]
+pub struct BulkDocs {
+    docs: Option<u64>,
 }
 
 #[skip_serializing_none]
@@ -77,4 +104,81 @@ pub struct TaskWithParent {
     #[serde(flatten)]
     task: Task,
     parent: Option<ParentTask>,
+}
+
+impl TaskData {
+    pub fn new((action, description): (String, Option<String>)) -> Option<Self> {
+        let description = match description {
+            Some(description) => description,
+            None => return None,
+        };
+
+        let action = match action.split_once(":") {
+            Some((target, action)) if target == "indices" => action,
+            _ => return None,
+        };
+
+        let kind = match action.splitn(3, "/").collect::<Vec<&str>>()[1..] {
+            [operation, kind] if operation == "write" => kind,
+            _ => return None,
+        };
+
+        let (is_shard, is_primary) = match kind.splitn(3, "[").collect::<Vec<&str>>() {
+            parts if parts.len() == 3 => (parts[1] == "s]", parts[2] == "p]"),
+            parts if parts.len() == 2 => (parts[1] == "s]", false),
+            parts if parts.len() == 1 => (false, false),
+            _ => return None,
+        };
+
+        let (docs, description) = match description
+            .trim_start_matches("requests[")
+            .split_once("], ")
+        {
+            Some((docs, description)) => (docs.parse::<u64>().ok(), description),
+            None => (None, description.as_str()),
+        };
+
+        match is_shard {
+            true => {
+                let (index, number): (String, Option<u32>) =
+                    match description.trim_start_matches("index[").split_once("][") {
+                        Some((index, number)) => (
+                            index.into(),
+                            number.trim_end_matches("]").parse::<u32>().ok(),
+                        ),
+                        None => (String::default(), None),
+                    };
+
+                Some(TaskData {
+                    index: Some(TaskIndex {
+                        name: Some(vec![index]),
+                        count: Some(1),
+                        bulk: None,
+                    }),
+                    shard: Some(TaskShard {
+                        number: number,
+                        primary: Some(is_primary),
+                        bulk: Some(BulkDocs { docs }),
+                    }),
+                })
+            }
+            false => {
+                let indices = description
+                    .trim_start_matches("indices[")
+                    .trim_end_matches("]")
+                    .split(",")
+                    .map(|s| s.trim().into())
+                    .collect::<Vec<String>>();
+
+                Some(TaskData {
+                    index: Some(TaskIndex {
+                        count: Some(indices.len() as u32),
+                        name: Some(indices),
+                        bulk: Some(BulkDocs { docs }),
+                    }),
+                    shard: None,
+                })
+            }
+        }
+    }
 }
