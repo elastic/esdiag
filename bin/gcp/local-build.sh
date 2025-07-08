b97396c7f046e4e79b1ffa8d02b21132c15e918f
Ryan Eno <ryan.eno@elastic.co>
2025-07-07 19:39:43 -0700

    Move services to GCP project

--- bin/gcp/local-build.sh
+++ bin/gcp/local-build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Builds the Docker image for the Elastic Stack Diagnostic Tool, tagging with current architecture and version
-# Requires Docker registry pre-authentication
+# Requires Docker registry pre-authentication with: gcloud auth configure-docker us-west1-docker.pkg.dev
 
 if [[ ! -f Cargo.toml ]]; then
     echo "Cargo.toml not found, run from repository root"
@@ -13,6 +13,6 @@ declare arch=$(uname -m)
 declare version=$(grep '^version =' Cargo.toml | sed 's/version = "\(.*\)"/\1/')
 
 docker build ~/Development/esdiag \
-    --tag "us-west2-docker.pkg.dev/elastic-customer-eng/esdiag/esdiag:latest-${arch}" \
-    --tag "us-west2-docker.pkg.dev/elastic-customer-eng/esdiag/esdiag:${version}-${arch}" \
+    --tag "us-west1-docker.pkg.dev/elastic-ce-tools/esdiag/esdiag:latest-${arch}" \
+    --tag "us-west1-docker.pkg.dev/elastic-ce-tools/esdiag/esdiag:${version}-${arch}" \
     --push
