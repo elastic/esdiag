9a7b96ad6ca68522626e26d5174fc6d5ed456d00
Ryan Eno <ryan.eno@elastic.co>
2025-09-13 16:16:22 -0700

    Add Cloud API support

--- bin/min-diag.sh
+++ bin/min-diag.sh
@@ -27,36 +27,89 @@
 
 # Use the encoded Elasticsearch API key for cluster authentication
 declare APIKEY=""
-# Use the Elasticsearch endpoint URL, no trailing slashes
+
+# Elasticsearch cluster URL, no trailing slashes
 declare URL=""
 
+# Elastic Cloud Admin deployment ID and API proxy url
+#declare DEPLOYMENT_ID=""
+#declare ADMIN_DOMAIN=""
+#declare URL="https://${ADMIN_DOMAIN}/api/v1/deployments/${DEPLOYMENT_ID}/elasticsearch/main-elasticsearch/proxy"
+
 # Seconds between each collection start, be sure to wait long enough not to overlap collections
-declare WAIT_TIME=60
-# Number of collections to perform, for example a 60 second wait * 60 collections = 30 minutes
-declare COLLECTION_COUNT=30
+declare WAIT_SECONDS=60
+# Number of collections to perform, for example a 60 second wait * 60 collections = 30 minute runtime
+declare COLLECTION_COUNT=5
+
+# ------ Logging Functions ------
+
+declare log_name="min-diag"
+
+# Only print colors if output is a terminal
+declare colorize=false
+if [[ -t 1 ]]; then
+    colorize=true
+fi
+
+function echo_color() {
+    local color=$1; shift
+    if [[ $colorize == true ]]; then
+        echo -e -n "\033[${color}m${*}\033[39m"
+    else
+        echo -n "${*}"
+    fi
+}
+
+# Colorized echo statements
+function black()     { echo_color 30 "${@}"; }
+function red()       { echo_color 31 "${@}"; }
+function green()     { echo_color 32 "${@}"; }
+function yellow()    { echo_color 33 "${@}"; }
+function blue()      { echo_color 34 "${@}"; }
+function magenta()   { echo_color 35 "${@}"; }
+function cyan()      { echo_color 36 "${@}"; }
+function gray()      { echo_color 90 "${@}"; }
+function lt_red()    { echo_color 91 "${@}"; }
+function lt_green()  { echo_color 92 "${@}"; }
+function lt_yellow() { echo_color 93 "${@}"; }
+function lt_blue()   { echo_color 94 "${@}"; }
+function lt_magenta(){ echo_color 95 "${@}"; }
+function lt_cyan()   { echo_color 96 "${@}"; }
+function white()     { echo_color 97 "${@}"; }
+
+# Colorized log messages
+function timestamp() { echo -n "$(date -u +"%Y-%m-%d %H:%M:%S")"; }
+function log_error() { echo "[$(timestamp) $(red Error) ${log_name}] ${*}"; }
+function log_warn()  { echo "[$(timestamp) $(yellow Warn)  ${log_name}] ${*}"; }
+function log_info()  { echo "[$(timestamp) $(green Info)  ${log_name}] ${*}"; }
+function log_debug() {
+    if [[ $LOG_LEVEL == "debug" ]]; then
+        echo "[$(timestamp) $(blue Debug) ${log_name}] ${*}"
+    fi
+}
 
 # ---------- Funcitons ----------
 
 # Prints script usage information
 function help() {
-    echo "Usage: $0 {watch|collect|help}"
+    white "Usage: $(green "$0" "<COMMAND>")"
     echo
-    echo "Commands:"
-    echo "  watch   - Collect diagnostics periodically based on the configured WAIT_TIME and COLLECTION_COUNT."
-    echo "  collect - Collect a single diagnostic immediately."
+    white "Commands:"
+    echo "  $(green watch)   - Collect diagnostics periodically based on the configured WAIT_SECONDS and COLLECTION_COUNT."
+    echo "  $(green collect) - Collect a single diagnostic immediately."
     echo
-    echo "Variables:"
-    echo "  APIKEY           - The encoded Elasticsearch API key for cluster authentication."
-    echo "  URL              - The Elasticsearch endpoint URL, no trailing slashes."
-    echo "  WAIT_TIME        - Seconds between each collection start."
-    echo "  COLLECTION_COUNT - Number of collections to perform."
+    white "Variables:"
+    echo "  $(green APIKEY)           - The encoded Elasticsearch API key for cluster authentication."
+    echo "  $(green URL)              - The Elasticsearch endpoint URL, no trailing slashes."
+    echo "  $(green WAIT_SECONDS)     - Seconds between each collection start."
+    echo "  $(green COLLECTION_COUNT) - Number of collections to perform."
     echo
 }
 
 # Saves a manifest file with metadata about the diagnostic collection.
 # It retrieves the Elasticsearch version from the `version.json` file.
 function save_manifest() {
-    local VERSION=$(jq -r '.version.number' version.json)
+    local VERSION && VERSION=$(jq -r '.version.number' "${DIR}/version.json")
     echo '{
     "mode" : "minimum",
     "product" : "elasticsearch",
@@ -64,59 +117,78 @@ function save_manifest() {
     "diagnostic" : null,
     "type" : "elasticsearch_diagnostic",
     "runner" : "min-diag",
-    "version" : "'$VERSION'",
-    "timestamp" : "'$(date -u +"%Y-%m-%dT%H:%M:%SZ")'"
-}' > diagnostic_manifest.json
+    "version" : "'"$VERSION"'",
+    "timestamp" : "'"$(date -u +"%Y-%m-%dT%H:%M:%SZ")"'"
+}' > "${DIR}/diagnostic_manifest.json"
+}
+
+# Check if curl command is available
+if ! command -v curl > /dev/null; then
+    log_error "$(red missing) required command $(white curl)."
+    exit 1
+else
+    log_debug "$(green found) command $(white curl)"
+fi
+
+function get_api() {
+    local API="$1"
+    local OUTPUT="$2"
+
+    log_info "$(green saving) $(cyan "${API}") to $(gray "${DIR}/${OUTPUT}")"
+    curl --silent \
+        --header "Authorization: ApiKey ${APIKEY}" \
+        --header "X-Management-Request: true" \
+        --output "$DIR/$OUTPUT" \
+        "$URL/$API"
 }
 
 # Collects API calls from the Elasticsearch cluster and saves them to files. It
 # creates a subdirectory with the current date and time, finally calling save_manifest.
 function collect_diag() {
-    local DATE=$(date +"%Y%m%d-%H%M%S")
-    local DIR="api-diagnostic-$DATE"
-    echo "$DIR"
+    local DATE && DATE=$(date +"%Y%m%d-%H%M%S")
+    declare DIR="api-diagnostic-$DATE"
+    log_info "$(green created) directory $(gray "$DIR")"
 
-    mkdir -p $DIR/commercial
-    cd $DIR
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_alias" -o alias.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_cluster/settings?include_defaults&flat_settings" -o cluster_settings_defaults.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_data_stream?expand_wildcards=all" -o commercial/data_stream.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/*/_ilm/explain?expand_wildcards=all" -o commercial/ilm_explain.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_ilm/policy" -o commercial/ilm_policies.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_settings?expand_wildcards=all" -o settings.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_stats?level=shards&expand_wildcards=all&ignore_unavailable=true" -o indices_stats.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_license" -o licenses.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_nodes" -o nodes.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_nodes/stats" -o nodes_stats.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_cluster/pending_tasks" -o cluster_pending_tasks.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_searchable_snapshots/cache/stats" -o commercial/searchable_snapshots_cache_stats.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_slm/policy" -o commercial/slm_policies.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/_tasks?detailed=true" -o tasks.json
-    curl --silent -H "Authorization: ApiKey $APIKEY" "$URL/" -o version.json
+    mkdir -p "$DIR/commercial"
+    get_api "/" version.json
+    get_api "_alias" alias.json
+    get_api "_cluster/settings?include_defaults&flat_settings" cluster_settings_defaults.json
+    get_api "_data_stream?expand_wildcards=all" commercial/data_stream.json
+    get_api "*/_ilm/explain?expand_wildcards=all" commercial/ilm_explain.json
+    get_api "_ilm/policy" commercial/ilm_policies.json
+    get_api "_settings?expand_wildcards=all" settings.json
+    get_api "_stats?level=shards&expand_wildcards=all&ignore_unavailable=true" indices_stats.json
+    get_api "_license" licenses.json
+    get_api "_nodes" nodes.json
+    get_api "_nodes/stats" nodes_stats.json
+    get_api "_cluster/pending_tasks" cluster_pending_tasks.json
+    get_api "_searchable_snapshots/cache/stats" commercial/searchable_snapshots_cache_stats.json
+    get_api "_slm/policy" commercial/slm_policies.json
+    get_api "_tasks?detailed=true" tasks.json
     save_manifest
-    cd ..
 }
 
 # Watches a cluster and periodically collects diagnostics. It runs collect_diag
-# COLLECTION_COUNT times, waiting WAIT_TIME seconds between each collection. This
+# COLLECTION_COUNT times, waiting WAIT_SECONDS seconds between each collection. This
 # outputs the new directory name at the start of each diagnostic collection.
 function watch() {
-    echo "Collecting $COLLECTION_COUNT diagnostics, $WAIT_TIME seconds apart, from $URL"
+    log_info "$(green collecting) $(cyan "$COLLECTION_COUNT") diagnostics, $(cyan "$WAIT_SECONDS") seconds apart, from $(blue $URL)"
     for i in $(seq 1 ${COLLECTION_COUNT}); do
-        echo -n "Collecting diag ${i}: "
+        log_info "$(green collecting) diagnostic $(cyan "$i") of $(cyan "$COLLECTION_COUNT")"
         # Running collections in the background allows sleep to start counting immediately
         collect_diag &
         # Skip the last sleep
-        if [ $i -lt ${COLLECTION_COUNT} ]; then
-            sleep ${WAIT_TIME}
+        if [[ "${i}" -lt "${COLLECTION_COUNT}" ]]; then
+            sleep ${WAIT_SECONDS}
         fi
     done
+    # Wait so the last background diagnostic doesn't exit before collections finish
+    wait $!
 }
 
 # Collects a single diagnostic from the cluster.
 function collect() {
-    echo "Collecting diagnostic from $URL"
-    echo -n "Saving diag ${i}: "
+    log_info "$(green collecting) diagnostic from $(blue "$URL")"
     collect_diag
 }
 
@@ -130,8 +202,7 @@ fi
 
 # Verify required variables were set
 if [[ -z "$APIKEY" || -z "$URL" ]]; then
-    echo "ERROR: Must set URL and APIKEY variables in the script."
-    echo
+    log_error "$(red missing) $(magenta URL) and $(magenta APIKEY) in the script."
     help
     exit 1
 fi
@@ -142,8 +213,7 @@ if [[ "$1" == "watch" ]]; then
 elif [[ "$1" == "collect" ]]; then
     collect
 else
-    echo "ERROR: No command given."
-    echo
+    log_error "$(red missing) command"
     help
     exit 1
 fi
