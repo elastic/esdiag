9b581ea9b09854cf5a3c0b1640ebf4cfa2d1589c
Ryan Eno <ryan.eno@elastic.co>
2025-10-21 09:27:19 -0700

    Update Kibana link

--- src/processor/mod.rs
+++ src/processor/mod.rs
@@ -267,7 +267,7 @@ impl Processor<Processing> {
                 x => &format!("from:now-{}d,to:now", x + 1),
             };
             let kibana_link = format!(
-                "{}/app/dashboards#/view/4e0a26b2-e5f8-4b58-b617-86f5cdd0edad?_g=(filters:!(('$state':(store:globalState),meta:(disabled:!f,index:'4319ebc4-df81-4b18-b8bd-6aaa55a1fd13',key:diagnostic.id,negate:!f,params:(query:'{}'),type:phrase),query:(match_phrase:(diagnostic.id:'{}')))),refreshInterval:(pause:!t,value:60000),time:({}))",
+                "{}/app/dashboards#/view/elasticsearch-cluster-report?_g=(filters:!(('$state':(store:globalState),meta:(disabled:!f,index:'4319ebc4-df81-4b18-b8bd-6aaa55a1fd13',key:diagnostic.id,negate:!f,params:(query:'{}'),type:phrase),query:(match_phrase:(diagnostic.id:'{}')))),refreshInterval:(pause:!t,value:60000),time:({}))",
                 kibana_url, url_safe_id, url_safe_id, time_filter
             );
             log::info!("{}", kibana_link);
