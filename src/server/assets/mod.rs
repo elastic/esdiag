c2f8b1d718ebc2cd4b6a4bdd1d02a8eb32f044b3
Ryan Eno <ryan.eno@elastic.co>
2026-02-27 21:26:21 -0800

    style: cleanup assets and organize templates after docs refactor

--- src/server/assets/mod.rs
+++ src/server/assets/mod.rs
@@ -2,11 +2,11 @@
 // or more contributor license agreements. Licensed under the Elastic License 2.0;
 // you may not use this file except in compliance with the Elastic License 2.0.
 
+use crate::embeds::ServerAssets;
 use axum::{
-    http::{header, StatusCode},
+    http::{StatusCode, header},
     response::IntoResponse,
 };
-use crate::embeds::{Assets, ServerAssets};
 
 fn serve_server_asset(path: &str, content_type: &'static str) -> impl IntoResponse {
     match ServerAssets::get(path) {
@@ -34,53 +34,22 @@ pub async fn style() -> impl IntoResponse {
     serve_server_asset("style.css", "text/css")
 }
 
-
 pub async fn prism() -> impl IntoResponse {
-    match Assets::get("prism.js") {
-        Some(content) => {
-            let body = content.data.into_owned();
-            ([(header::CONTENT_TYPE, "text/javascript")], body).into_response()
-        }
-        None => StatusCode::NOT_FOUND.into_response(),
-    }
+    serve_server_asset("prism.js", "text/javascript")
 }
 
 pub async fn prism_bash() -> impl IntoResponse {
-    match Assets::get("prism-bash.js") {
-        Some(content) => {
-            let body = content.data.into_owned();
-            ([(header::CONTENT_TYPE, "text/javascript")], body).into_response()
-        }
-        None => StatusCode::NOT_FOUND.into_response(),
-    }
+    serve_server_asset("prism-bash.js", "text/javascript")
 }
 
 pub async fn prism_json() -> impl IntoResponse {
-    match Assets::get("prism-json.js") {
-        Some(content) => {
-            let body = content.data.into_owned();
-            ([(header::CONTENT_TYPE, "text/javascript")], body).into_response()
-        }
-        None => StatusCode::NOT_FOUND.into_response(),
-    }
+    serve_server_asset("prism-json.js", "text/javascript")
 }
 
 pub async fn prism_json5() -> impl IntoResponse {
-    match Assets::get("prism-json5.js") {
-        Some(content) => {
-            let body = content.data.into_owned();
-            ([(header::CONTENT_TYPE, "text/javascript")], body).into_response()
-        }
-        None => StatusCode::NOT_FOUND.into_response(),
-    }
+    serve_server_asset("prism-json5.js", "text/javascript")
 }
 
 pub async fn prism_css() -> impl IntoResponse {
-    match Assets::get("prism.css") {
-        Some(content) => {
-            let body = content.data.into_owned();
-            ([(header::CONTENT_TYPE, "text/css")], body).into_response()
-        }
-        None => StatusCode::NOT_FOUND.into_response(),
-    }
+    serve_server_asset("prism.css", "text/css")
 }
